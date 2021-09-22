39ff213570a5ad6a4cec2509378ed42698f9ff67
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2021-09-22 15:42:16 +0200

    Changes to both files & new .txt file

--- src/hu/petrik/emberoop/Main.java
+++ src/hu/petrik/emberoop/Main.java
@@ -3,6 +3,8 @@ package hu.petrik.emberoop;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Ember e1 = new Ember("Lakatos Ferenc", "1983-1-12", "Budapest");
+	Ember e2 = new Ember("Kovács Béla", "1977-7-17", "Budapest");
+	Ember e3 = new Ember("Erős István", "1981-6-22", "Debrecen");
     }
 }

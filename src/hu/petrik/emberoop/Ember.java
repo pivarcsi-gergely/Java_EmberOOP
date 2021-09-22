6b402087521824dcfa199be2d9ea95b04ab2cf68
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2021-09-22 16:16:48 +0200

    Szül. hónap, nap és életkor funkciók

--- src/hu/petrik/emberoop/Ember.java
+++ src/hu/petrik/emberoop/Ember.java
@@ -1,5 +1,8 @@
 package hu.petrik.emberoop;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Ember {
     private String nev;
     private String szulDatum;
@@ -15,6 +18,22 @@ public class Ember {
         String szuletesiEvStringben = szulDatum.substring(0,4);
         return Integer.parseInt(szuletesiEvStringben);
     }
+
+    public int getSzuletesiHonap(){
+        String[] szuletesiAdatok = this.szulDatum.split("-");
+        return Integer.parseInt(szuletesiAdatok[1]);
+    }
+    public int getSzuletesiNap(){
+        String[] szuletesiAdatok = this.szulDatum.split("-");
+        return Integer.parseInt(szuletesiAdatok[2]);
+    }
+    public int getEletkor(){
+        LocalDate maiDatum = LocalDate.now();
+        LocalDate szuletesiDatum = LocalDate.parse(szulDatum);
+        Period kulonbseg = Period.between(maiDatum, szuletesiDatum);
+        return kulonbseg.getYears();
+    }
+
 
 
     @Override

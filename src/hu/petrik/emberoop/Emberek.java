b0bbaec1092ab7e13ce2453070950aed58b7e24d
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2021-09-23 11:13:55 +0200

    Emberekben konstruktor: statikus tömbbel tölti fel a listát

--- src/hu/petrik/emberoop/Emberek.java
+++ src/hu/petrik/emberoop/Emberek.java
@@ -1,6 +1,7 @@
 package hu.petrik.emberoop;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Emberek {
@@ -8,5 +9,9 @@ public class Emberek {
 
     public Emberek(Ember[] emberTomb) {
         this.emberLista = new ArrayList<>();
+        /*this.emberLista.addAll(Arrays.asList(emberTomb)); : addAll; a tömböt listává konvertálja viszont*/
+        for (Ember ember: emberTomb) {
+            this.emberLista.add(ember);
+        }
     }
 }

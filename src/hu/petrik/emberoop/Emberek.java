5483bd05a1aa750a9072e9c43445cfd0076dd17e
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2021-09-23 11:09:54 +0200

    Új Emberek osztály+

--- src/hu/petrik/emberoop/Emberek.java
+++ src/hu/petrik/emberoop/Emberek.java
@@ -1,7 +1,12 @@
 package hu.petrik.emberoop;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Emberek {
     private List<Ember> emberLista;
+
+    public Emberek(Ember[] emberTomb) {
+        this.emberLista = new ArrayList<>();
+    }
 }

9eabeae0cb54b43a04b86f503a12d77bffad949c
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2021-09-23 11:26:11 +0200

    Emberek példányosítása

--- src/hu/petrik/emberoop/Main.java
+++ src/hu/petrik/emberoop/Main.java
@@ -6,6 +6,20 @@ public class Main {
 	Ember e1 = new Ember("Lakatos Ferenc", "1983-1-12", "Budapest");
 	Ember e2 = new Ember("Kovács Béla", "1977-7-17", "Budapest");
 	Ember e3 = new Ember("Erős István", "1981-6-22", "Debrecen");
+
+
+	emberOsztalyTeszje(e1, e2, e3);
+
+	Ember[] emberTomb = new Ember[]{e1,e2,e3};
+
+	Emberek eK1 = new Emberek(emberTomb);
+	System.out.println(eK1);
+
+
+
+    }
+
+    public static void emberOsztalyTeszje(Ember e1, Ember e2, Ember e3){
 		System.out.println(e1);
 		System.out.println(e2);
 		System.out.println(e3);
@@ -15,5 +29,5 @@ public class Main {
 		System.out.println("Teszt");
 
 		e1.getSzuletesiEv();
-    }
+	}
 }

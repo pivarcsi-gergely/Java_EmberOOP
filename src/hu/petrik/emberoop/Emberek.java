4f42726ae96c2246dafabcbc7136620f2ee1a9a8
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2021-09-23 12:53:35 +0200

    Emberekbe import

--- src/hu/petrik/emberoop/Emberek.java
+++ src/hu/petrik/emberoop/Emberek.java
@@ -1,6 +1,8 @@
 package hu.petrik.emberoop;
 
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
